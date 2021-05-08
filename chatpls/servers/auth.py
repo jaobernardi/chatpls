29aa72a043c04ada6814ae92589bffc520b32492
Jão's workspace <joao_bernardi@outlook.com>
2021-05-08 00:59:04 -0300

    Finish auth flow

--- chatpls/servers/auth.py
+++ chatpls/servers/auth.py
@@ -3,7 +3,11 @@ from structures import Response, Config
 import requests
 import json
 
-
+errors = {
+	404: b"",
+	403: b"",
+	500: b"",
+	}
 config = Config()
 
 @events.add_handle("http_request")
@@ -13,18 +17,40 @@ def auth_http(event):
 		default_headers = {
 			"Server": "chatpls/1.0",
 		}
-		if "code" in request.query_string:
-			twitch_query = requests.post(f'https://id.twitch.tv/oauth2/token?client_id=r9fxp28e0wimgjdpf9dg050ncn7spi&client_secret={config.client_secret}&code={request.query_string["code"]}&grant_type=authorization_code&redirect_uri=https://auth.chatpls.live')
-	
-			return Response.make(
-				302,
-				'Found',
-				default_headers | {'Location': 'https://chatpls.live/'},
-				json.dumps(twitch_query.json()).encode()
-			)
-		return Response.make(
-			302,
-			'Found',
-			default_headers | {'Location': "https://id.twitch.tv/oauth2/authorize?client_id=r9fxp28e0wimgjdpf9dg050ncn7spi&redirect_uri=https://auth.chatpls.live&response_type=code&scope=openid user:read:subscriptions user:read:follows"},
-			b""
-		)
+		path = [i for i in request.path.split("/")[1:] if i]
+		match path:
+			case []:
+				if "code" in request.query_string:
+					twitch_query = requests.post(f'https://id.twitch.tv/oauth2/token?client_id=r9fxp28e0wimgjdpf9dg050ncn7spi&client_secret={config.client_secret}&code={request.query_string["code"]}&grant_type=authorization_code&redirect_uri=https://auth.chatpls.live')
+			
+					return Response.make(
+						302,
+						'Found',
+						default_headers | {'Location': 'https://chatpls.live/'},
+						json.dumps(twitch_query.json()).encode()
+					)
+				return Response.make(
+					302,
+					'Found',
+					default_headers | {'Location': "https://id.twitch.tv/oauth2/authorize?client_id=r9fxp28e0wimgjdpf9dg050ncn7spi&redirect_uri=https://auth.chatpls.live&response_type=code&scope=openid user:read:subscriptions user:read:follows"},
+					b""
+				)
+			case ["tokens", *params]:
+				message = b'{"status": 200, "message": "reached incoming path.", "error": false}'
+				return Response.make(
+					200,
+					'OK',
+					default_headers | {'Content-Type': 'application/json',
+					'Content-Length': len(message)},
+					message
+				)
+			case _:
+				return Response.make(
+					403,
+					'Unauthorized', 
+					default_headers|{
+						'Content-Type': 'text/html',
+						'Content-Length': len(errors[403])
+					},
+					errors[403]
+				)
