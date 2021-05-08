6d73e000a955cc3a54747ce91b0bafd567f9be2f
Jão's workspace <joao_bernardi@outlook.com>
2021-05-07 21:54:57 -0300

    Ident

--- chatpls/servers/fallback.py
+++ chatpls/servers/fallback.py
@@ -6,13 +6,13 @@ config = Config()
 @events.add_handle("http_request", priority=-1)
 def fallback_http(event):
 	request = event.request
-    default_headers = {
-        "Server": "chatpls/1.0",
-    }
-    return Response.make(
-        200,
-        'OK',
-        default_headers,
-        b"If you're reading this, you fucked up."
-    )
+	default_headers = {
+		"Server": "chatpls/1.0",
+	}
+	return Response.make(
+		200,
+		'OK',
+		default_headers,
+		b"If you're reading this, you fucked up."
+	)
 		
