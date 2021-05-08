6a32f92b969fa2f3860c24ef0bff5cd739f4a8d5
Jão's workspace <joao_bernardi@outlook.com>
2021-05-08 07:47:00 -0300

    Porting to Python 3.9

--- chatpls/servers/api.py
+++ chatpls/servers/api.py
@@ -10,24 +10,23 @@ def api_http(event):
 		default_headers = {
 			"Server": "chatpls/1.0",
 		}
-		path = [i for i in request.path.split("/")[1:] if i]
+		path = [i.lower() for i in request.path.split("/")[1:] if i]
 		# check api-method.
-		match path:
-			case ["incoming", *params]:
-				message = b'{"status": 200, "message": "reached incoming path.", "error": false}'
-				return Response.make(
-					200,
-					'OK',
-					default_headers | {'Content-Type': 'application/json',
-					'Content-Length': len(message)},
-					message
-				)
-			case _:
-				message = b'{"status": 200, "message": "ok.", "error": false}'
-				return Response.make(
-					200,
-					'OK',
-					default_headers | {'Content-Type': 'application/json',
-					'Content-Length': len(message)},
-					message
-				)
+		if path[0] == "incoming":
+			message = b'{"status": 200, "message": "reached incoming path.", "error": false}'
+			return Response.make(
+				200,
+				'OK',
+				default_headers | {'Content-Type': 'application/json',
+				'Content-Length': len(message)},
+				message
+			)
+		else:
+			message = b'{"status": 200, "message": "ok.", "error": false}'
+			return Response.make(
+				200,
+				'OK',
+				default_headers | {'Content-Type': 'application/json',
+				'Content-Length': len(message)},
+				message
+			)
--- chatpls/servers/auth.py
+++ chatpls/servers/auth.py
@@ -17,40 +17,39 @@ def auth_http(event):
 		default_headers = {
 			"Server": "chatpls/1.0",
 		}
-		path = [i for i in request.path.split("/")[1:] if i]
-		match path:
-			case []:
-				if "code" in request.query_string:
-					twitch_query = requests.post(f'https://id.twitch.tv/oauth2/token?client_id=r9fxp28e0wimgjdpf9dg050ncn7spi&client_secret={config.client_secret}&code={request.query_string["code"]}&grant_type=authorization_code&redirect_uri=https://auth.chatpls.live')
-			
-					return Response.make(
-						200,
-						'OK',
-						default_headers | {'Content-Type': 'application/json'},
-						json.dumps(twitch_query.json()).encode()
-					)
+		path = [i.lower() for i in request.path.split("/")[1:] if i]
+		if path == []:
+			if "code" in request.query_string:
+				twitch_query = requests.post(f'https://id.twitch.tv/oauth2/token?client_id=r9fxp28e0wimgjdpf9dg050ncn7spi&client_secret={config.client_secret}&code={request.query_string["code"]}&grant_type=authorization_code&redirect_uri=https://auth.chatpls.live')
+		
 				return Response.make(
-					302,
-					'Found',
-					default_headers | {'Location': "https://id.twitch.tv/oauth2/authorize?client_id=r9fxp28e0wimgjdpf9dg050ncn7spi&redirect_uri=https://auth.chatpls.live&response_type=code&scope=openid user:read:subscriptions user:read:follows"},
-					b""
+					200,
+					'OK',
+					default_headers | {'Content-Type': 'application/json'},
+					json.dumps(twitch_query.json()).encode()
 				)
-			case ["tokens", *params]:
-				message = b'{"status": 501, "message": "Not Implemented", "error": true}'
-				return Response.make(
-					501,
-					'Not Implemented',
-					default_headers | {'Content-Type': 'application/json',
-					'Content-Length': len(message)},
-					message
-				)
-			case _:
-				return Response.make(
-					403,
-					'Unauthorized', 
-					default_headers|{
-						'Content-Type': 'text/html',
-						'Content-Length': len(errors[403])
-					},
-					errors[403]
-				)
+			return Response.make(
+				302,
+				'Found',
+				default_headers | {'Location': "https://id.twitch.tv/oauth2/authorize?client_id=r9fxp28e0wimgjdpf9dg050ncn7spi&redirect_uri=https://auth.chatpls.live&response_type=code&scope=openid user:read:subscriptions user:read:follows"},
+				b""
+			)
+		elif path[0] == ["tokens"]:
+			message = b'{"status": 501, "message": "Not Implemented", "error": true}'
+			return Response.make(
+				501,
+				'Not Implemented',
+				default_headers | {'Content-Type': 'application/json',
+				'Content-Length': len(message)},
+				message
+			)
+		else:
+			return Response.make(
+				403,
+				'Unauthorized', 
+				default_headers|{
+					'Content-Type': 'text/html',
+					'Content-Length': len(errors[403])
+				},
+				errors[403]
+			)
