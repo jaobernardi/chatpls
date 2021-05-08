2fd91f52b834180f8dff74a16aa3dab8b6ec6abf
Jão's workspace <joao_bernardi@outlook.com>
2021-05-07 21:47:29 -0300

    Ident fix

--- chatpls/servers/auth.py
+++ chatpls/servers/auth.py
@@ -4,12 +4,12 @@ from structures import Response, Config
 config = Config()
 
 @events.add_handle("http_request")
-def api_http(event):
+def auth_http(event):
 	request = event.request
 	if "Host" in request.headers and request.headers["Host"] == config.scopes["api"]:
 		default_headers = {
 			"Server": "chatpls/1.0",
 		}
 		print(request.query_string)
-        print(request.method)
-        print(request.data)
+		print(request.method)
+		print(request.data)
