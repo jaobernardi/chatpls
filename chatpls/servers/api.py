5251486dfdd4a3ff9221cdf2f0617bad0ae4fcda
Jão's workspace <joao_bernardi@outlook.com>
2021-05-07 21:46:10 -0300

    Auth tests

---
+++ chatpls/servers/api.py
@@ -0,0 +1,33 @@
+import events
+from structures import Response, Config
+
+config = Config()
+
+@events.add_handle("http_request")
+def api_http(event):
+	request = event.request
+	if "Host" in request.headers and request.headers["Host"] == config.scopes["api"]:
+		default_headers = {
+			"Server": "chatpls/1.0",
+		}
+		path = [i for i in request.path.split("/")[1:] if i]
+		# check api-method.
+		match path:
+			case ["incoming", *params]:
+				message = b'{"status": 200, "message": "reached incoming path.", "error": false}'
+				return Response.make(
+					200,
+					'OK',
+					default_headers | {'Content-Type': 'application/json',
+					'Content-Length': len(message)},
+					message
+				)
+			case _:
+				message = b'{"status": 200, "message": "ok.", "error": false}'
+				return Response.make(
+					200,
+					'OK',
+					default_headers | {'Content-Type': 'application/json',
+					'Content-Length': len(message)},
+					message
+				)
--- chatpls/servers/auth.py
+++ chatpls/servers/auth.py
@@ -4,30 +4,12 @@ from structures import Response, Config
 config = Config()
 
 @events.add_handle("http_request")
-def normal_http(event):
+def api_http(event):
 	request = event.request
 	if "Host" in request.headers and request.headers["Host"] == config.scopes["api"]:
 		default_headers = {
 			"Server": "chatpls/1.0",
 		}
-		path = [i for i in request.path.split("/")[1:] if i]
-		# check api-method.
-		match path:
-			case ["incoming", *params]:
-				message = b'{"status": 200, "message": "reached incoming path.", "error": false}'
-				return Response.make(
-					200,
-					'OK',
-					default_headers | {'Content-Type': 'application/json',
-					'Content-Length': len(message)},
-					message
-				)
-			case _:
-				message = b'{"status": 200, "message": "ok.", "error": false}'
-				return Response.make(
-					200,
-					'OK',
-					default_headers | {'Content-Type': 'application/json',
-					'Content-Length': len(message)},
-					message
-				)
+		print(request.query_string)
+        print(request.method)
+        print(request.data)
