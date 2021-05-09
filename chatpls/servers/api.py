3707832a1a290d5bdaf0bc1fc0315678d9b80c6e
Jão's workspace <joao_bernardi@outlook.com>
2021-05-09 07:33:09 -0300

    Api development

--- chatpls/servers/api.py
+++ chatpls/servers/api.py
@@ -7,7 +7,7 @@ twitch = TwitchAPI('r9fxp28e0wimgjdpf9dg050ncn7spi', config.client_secret)
 queue = []
 current = {
 	'data': {'link': '', 'likes': 0, 'dislikes': 0, 'time': 0},
-	'stats': {}
+	'voters': {}
 }
 
 @events.add_handle("http_request")
@@ -24,9 +24,9 @@ def api_http(event):
 					default_headers = default_headers | {'Allow': 'GET'}
 					output = {"status": 405, "message": "Method Not Allowed", "error": True}
 				elif not current:
-					output = {"status": 404, "message": "Not Found", "error": True, "data": current}
+					output = {"status": 404, "message": "Not Found", "error": True, "data": current['data']}
 				else:
-					output = {"status": 200, "message": "OK", "error": False, "data": current}
+					output = {"status": 200, "message": "OK", "error": False, "data": current['data']}
 
 			case ["current", "reaction"]:
 				if request.method != "POST":					
@@ -35,43 +35,30 @@ def api_http(event):
 				elif not current:					
 					output = {"status": 404, "message": "Not Found", "error": True}
 				elif 'Content-Type' in request.headers and request.headers['Content-Type'] == 'application/json':
-					if True:
+					try:
 						data = json.loads(request.data.decode("utf-8"))
 						match data:
 							case {"token": token, "action_id": action_id}:
 								with Database() as db:
 									users = db.get_tokens(token=token)	
-								print(users)												
+									
 								if users:
-									if users[0] not in current["stats"]:
-										current["stats"][users[0]] = action_id
-										if action_id == 0:
-											current["data"]["likes"] += 1
-										elif action_id == 1:
-											current["data"]["dislikes"] += 1
-										else:
-											output = {"status": 422, "message": "Unprocessable Entity", "error": True}
-									if current["stats"][users[0]] == action_id:										
+									if users[0] not in current["voters"]:
 										output = {"status": 200, "message": "OK", "error": False}
-									else:
-										if action_id == 0 and current["stats"][users[0]] == 1:
-											current["data"]["dislikes"] -= 1
-											current["data"]["likes"] += 1
-											output = {"status": 200, "message": "OK", "error": False}
-										elif action_id == 1 and current["stats"][users[0]] == 0:
-											current["data"]["dislikes"] += 1
-											current["data"]["likes"] -= 1
-
-											output = {"status": 200, "message": "OK", "error": False}
+										if action_id == 0:
+											current['data']['likes'] += 1
+										elif action_id == 1:
+											current['data']['dislikes'] += 1
 										else:
 											output = {"status": 422, "message": "Unprocessable Entity", "error": True}
+									else:
+										output = {"status": 403, "message": "Unauthorized", "error": True}
 								else:
 									output = {"status": 403, "message": "Unauthorized", "error": True}
 							case _:
 								output = {"status": 422, "message": "Unprocessable Entity", "error": True}
-					#except Exception as e:
-					#	print(e)						
-					#	output = {"status": 422, "message": "Unprocessable Entity", "error": True}
+					except Exception as e:					
+						output = {"status": 422, "message": "Unprocessable Entity", "error": True}
 				else:							
 					output = {"status": 422, "message": "Unprocessable Entity", "error": True}
 				
