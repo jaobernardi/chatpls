7fd1ad6d8d4a14056f0ffba459126c26bd439de3
Jão's workspace <joao_bernardi@outlook.com>
2021-05-09 04:50:38 -0300

    Twitch finish

--- chatpls/structures/twitch.py
+++ chatpls/structures/twitch.py
@@ -21,7 +21,24 @@ class TwitchAPI:
 			return userinfo_data(user)
 		return request.json()
 
-	def get_user_follows(self, access_token, refresh_token, id, channel_id):
+	def check_follow(self, user: User):
+		request = request.get(f"https://api.twitch.tv/helix/users/follows?from_id={user.user_id}&to_id=28579002")
+		if request.status_code != 200:
+			new_access_token, new_refresh_token = self.refresh_access_token(user.refresh_token)
+			user.update_access_token(new_access_token, new_refresh_token)
+			return check_follow(user)
+		request = request.json()
+		if request['total'] > 0:
+			return True
 		return False
+
 	def get_user_subscription_status(self, access_token, refresh_token, id):
+		request = request.get(f"https://api.twitch.tv/helix/subscriptions/user?user_id={user.user_id}&broadcaster_id=28579002")
+		if request.status_code not in [200, 404]:
+			new_access_token, new_refresh_token = self.refresh_access_token(user.refresh_token)
+			user.update_access_token(new_access_token, new_refresh_token)
+			return check_follow(user)
+
+		if request.status_code == 200:
+			return True
 		return False
