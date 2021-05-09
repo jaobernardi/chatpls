15c022ddc11444574bb39e3f4a2b2740d349036a
Jão's workspace <joao_bernardi@outlook.com>
2021-05-09 04:53:25 -0300

    Twitch finish

--- chatpls/structures/twitch.py
+++ chatpls/structures/twitch.py
@@ -22,7 +22,7 @@ class TwitchAPI:
 		return request.json()
 
 	def check_follow(self, user: User):
-		request = request.get(f"https://api.twitch.tv/helix/users/follows?from_id={user.user_id}&to_id=28579002")
+		request = request.get(f"https://api.twitch.tv/helix/users/follows?from_id={user.user_id}&to_id=28579002", headers={"Authorization": f"Bearer {user.access_token}", "Client-Id": self.client_id})
 		if request.status_code != 200:
 			new_access_token, new_refresh_token = self.refresh_access_token(user.refresh_token)
 			user.update_access_token(new_access_token, new_refresh_token)
@@ -33,7 +33,7 @@ class TwitchAPI:
 		return False
 
 	def get_user_subscription_status(self, access_token, refresh_token, id):
-		request = request.get(f"https://api.twitch.tv/helix/subscriptions/user?user_id={user.user_id}&broadcaster_id=28579002")
+		request = request.get(f"https://api.twitch.tv/helix/subscriptions/user?user_id={user.user_id}&broadcaster_id=28579002", headers={"Authorization": f"Bearer {user.access_token}", "Client-Id": self.client_id})
 		if request.status_code not in [200, 404]:
 			new_access_token, new_refresh_token = self.refresh_access_token(user.refresh_token)
 			user.update_access_token(new_access_token, new_refresh_token)
@@ -42,3 +42,4 @@ class TwitchAPI:
 		if request.status_code == 200:
 			return True
 		return False
+	
