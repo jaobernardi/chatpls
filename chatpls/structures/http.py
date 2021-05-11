ed7b1b2aec1979360c0a340e2a774b7e27f5ae42
Jão's workspace <joao_bernardi@outlook.com>
2021-05-10 21:31:50 -0300

    Timeout testings

--- chatpls/structures/http.py
+++ chatpls/structures/http.py
@@ -36,7 +36,6 @@ class Server(object):
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.bind((self.host, self.port))
 		self.socket.listen(50)
-		self.socket.settimeout(5)
 		# Start connection loop
 		with self.context.wrap_socket(self.socket, server_side=True) as ssock:
 			while True:
