687000d5fc113fd71be96517a30f4ff10c22f709
Jão's workspace <joao_bernardi@outlook.com>
2021-05-12 04:21:33 -0300

    Json api

--- chatpls/web_assets/assets/api_interface.js
+++ chatpls/web_assets/assets/api_interface.js
@@ -6,8 +6,22 @@ function get_current(callback){
 
     Http.onreadystatechange = (e) => {
         response = Http.responseText
-        if (response.status == 200){
-            callback(response.link, response.start_time)
-        } 
+        if (response.status == 200 && response.data){
+            callback(response.data.link, response.data.start_time)
+        } else {
+            callback(null, null)
+        }
+    }
+}
+
+function get_queue(callback){
+    Http = new XMLHttpRequest();
+    url='https://api.chatpls.live/current/';
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        response = Http.responseText
+        callback(response.data)
     }
 }
