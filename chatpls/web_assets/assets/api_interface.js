c50e9f36cd619b30e18a2e701a46aab3660f1173
Jão's workspace <joao_bernardi@outlook.com>
2021-05-12 06:15:47 -0300

    Api

--- chatpls/web_assets/assets/api_interface.js
+++ chatpls/web_assets/assets/api_interface.js
@@ -3,18 +3,22 @@ function get_current(callback){
     url='https://api.chatpls.live/current/';
     Http.open("GET", url);
     Http.send();
-    response = Http.responseText
-    try {
-        response = JSON.parse(response)
-    } catch (error) {
-        response = {status: 500, data: null}
+
+    Http.onreadystatechange = (e) => {
+        if (xmlHttp.readyState == 4){
+            response = Http.responseText
+            try {
+                response = JSON.parse(response)
+            } catch (error) {
+                response = {status: 500, data: null}
+            }
+            if (response.status == 200 && response.data){
+                callback(response.data.link, response.data.start_time)
+            } else {
+                callback(null, null)
+            }
+        }
     }
-    if (response.status == 200 && response.data){
-        callback(response.data.link, response.data.start_time)
-    } else {
-        callback(null, null)
-    }
-    
 }
 
 function get_queue(callback){
@@ -22,13 +26,18 @@ function get_queue(callback){
     url='https://api.chatpls.live/queue/';
     Http.open("GET", url);
     Http.send();
-    response = Http.responseText
-    try {
-        response = JSON.parse(response)
-    } catch (error) {
-        response = {queue: []}
-    }
-    
-    callback(response.queue)
 
+    Http.onreadystatechange = (e) => {
+        if (xmlHttp.readyState == 4){
+            response = Http.responseText
+            try {
+                response = JSON.parse(response)
+            } catch (error) {
+                response = {queue: []}
+            }
+            
+            callback(response.queue)
+    
+        }
+    }
 }
