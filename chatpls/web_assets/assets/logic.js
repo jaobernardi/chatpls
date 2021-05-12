cda4298f3d8634fbf33b511497cae266edca5f85
Jão's workspace <joao_bernardi@outlook.com>
2021-05-12 04:34:31 -0300

    Api

--- chatpls/web_assets/assets/logic.js
+++ chatpls/web_assets/assets/logic.js
@@ -7,9 +7,14 @@ function queue_item_element(text) {
 function update_list(queue) {
     queue_element = document.getElementById("queue")
     queue_element.innerHTML = ""
-    queue.forEach(element => {
-        queue_element.appendChild(queue_item_element(element.username))
-    });
+    if (queue){
+        queue.forEach(element => {
+            queue_element.appendChild(queue_item_element(element.username))
+        });
+    } else {
+        queue_element.appendChild("Nada por aqui . . . ")
+    }
+
 }
 
 setInterval(()=>{
