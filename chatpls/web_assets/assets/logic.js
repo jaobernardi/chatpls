74fffea6c3990fe9ff73946364f5e10eee9fd939
Jão's workspace <joao_bernardi@outlook.com>
2021-05-12 04:38:42 -0300

    Api

--- chatpls/web_assets/assets/logic.js
+++ chatpls/web_assets/assets/logic.js
@@ -1,5 +1,12 @@
+function image_element(link){
+    element = document.createElement("img")
+    element.href = link
+    return element
+}
+
 function queue_item_element(text) {
     element = document.createElement("h4")
+    element.align = "center"
     element.innerText = text
     return element
 }
@@ -12,7 +19,9 @@ function update_list(queue) {
             queue_element.appendChild(queue_item_element(element.username))
         });
     } else {
-        queue_element.appendChild(queue_item_element("Nada por aqui . . . "))
+        no_one = queue_item_element("Nada por aqui . . . ")
+        no_one.innerHTML += image_element("https://cdn.betterttv.net/emote/5d7eefb7c0652668c9e4d394/1x")
+        queue_element.appendChild(no_one)
     }
 
 }
